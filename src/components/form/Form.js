c54929b39f044083074b5aceec28a2c5dbda32b9
vtendero <tendero.valle@gmail.com>
2021-02-21 12:59:05 +0100

    create share component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../stylesheets/form/form.scss";
 import Design from "./Design";
 import Collapsable from './Collapsable';
+import Share from './Share';
 
 function Form() {
   return (
@@ -103,27 +104,8 @@ function Form() {
         name = 'Comparte'
         icon='collapsable__header--icon fas fa-share-alt'
        />
-        <div className='share-container collapsable__box js-collapsable-close'>
-          <div className='collapsable__share js-createbtn-container'>
-            <i className='collapsable__share--icon far fa-address-card'></i>
-            <a href='#' className='js-create-btn'>
-              Crear tarjeta
-            </a>
-            <div className='js-card-result hidden'></div>
-          </div>
-        </div>
-        <div className='share-preview-container'></div>
-        <div className='share-card hidden js-share__container'>
-          <h2 className='share-card__title js-created'>
-            La tarjeta ha sido creada:
-          </h2>
-          <a className='share-card__link js-share__link' href=''></a>
-          <button className='share-card__button js-twitter-btn'>
-            <a className='js-twitter-link' href='' target='blank'>
-              <i className='fab fa-twitter'></i>Compartir en Twitter
-            </a>
-          </button>
-        </div>
+      <Share />
+        
       </section>
     </form>
   );

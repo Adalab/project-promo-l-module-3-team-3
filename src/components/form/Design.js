53e5d1c66917a222f9dd22e201162cb1594c36fc
vtendero <tendero.valle@gmail.com>
2021-02-22 12:58:53 +0100

    change collapsable location

--- src/components/form/Design.js
+++ src/components/form/Design.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "../../stylesheets/form/design.scss";
+import Collapsable from './Collapsable';
 
 function Design() {
     return (
         <section className='collapsable'>
-            <div className='header-container collapsable__content js-collapsable'>
-                <div className='collapsable__header'>
-                    <i className='collapsable__header--icon far fa-object-ungroup'></i>
-                    <h2 className='collapsable__header--title'>Diseña</h2>
-                </div>
-                <div className='collapsable__header2'>
-                    <i className='collapsable__header--arrow fas fa-chevron-down js-collapsable-arrow'></i>
-                </div>
-            </div>
+            <Collapsable
+                name = 'Diseña'
+                icon='collapsable__header--icon far fa-object-ungroup'
+            />
             <div className='collapsable__box js-collapsable-close'>
                 <div className='collapsable__colors'>
                     <h3 className='collapsable__colors--title'>Colores</h3>

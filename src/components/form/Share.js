97ea5f33eb98153e2f853eda30cea47d87fb3aa6
marina-fp <marina.fdezpas@gmail.com>
2021-02-27 17:39:12 +0100

    Modify Share structure

--- src/components/form/Share.js
+++ src/components/form/Share.js
@@ -2,29 +2,24 @@ import "../../stylesheets/form/share.scss";
 import Collapsable from './Collapsable';
 
 const Share = (props) =>
-    <section className='collapsable collapsable3'>
-        <div className='share-container collapsable__box js-collapsable-close'>
-            <div className='collapsable__share js-createbtn-container'>
-                <i className='collapsable__share--icon far fa-address-card'></i>
-                <a href='#' className='js-create-btn'>
+    <section className='collapsable collapsable3 share__container collapsable__box js-collapsable-close'>
+            <div className='share__button js-createbtn-container'>
+                <i className='share__button--icon far fa-address-card'></i>
+                <a href='#' className='share__button--text js-create-btn'>
                 Crear tarjeta
                 </a>
-                <div className='js-card-result hidden'></div>
+            </div>            
+            <div className='share-card hidden js-share__container js-card-result'>
+                <h2 className='share-card__text js-created'>
+                        La tarjeta ha sido creada:
+                </h2>
+                <a className='share-card__link js-share__link' href=''></a>
+                <button className='share-card__button js-twitter-btn'>
+                    <a className='js-twitter-link' href='' target='blank'>
+                        <i className='fab fa-twitter'></i> Compartir en Twitter
+                    </a>
+                </button>
             </div>
-            </div>
-            <div className='share-preview-container'></div>
-            <div className='share-card hidden js-share__container'>
-            <h2 className='share-card__title js-created'>
-                La tarjeta ha sido creada:
-            </h2>
-            <a className='share-card__link js-share__link' href=''></a>
-            <button className='share-card__button js-twitter-btn'>
-                <a className='js-twitter-link' href='' target='blank'>
-                <i className='fab fa-twitter'></i>Compartir en Twitter
-                </a>
-            </button>
-        </div>
     </section>
  
-
 export default Share;

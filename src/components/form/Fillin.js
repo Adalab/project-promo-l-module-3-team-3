eac8b395bf88d83bd41c45fc6d00927f21cb70e8
Laura Portillo <lauraportillo.ark@gmail.com>
2021-02-27 17:37:02 +0100

    update

--- src/components/form/Fillin.js
+++ src/components/form/Fillin.js
@@ -1,86 +1,86 @@
 import React from 'react';
 import '../../stylesheets/form/fillin.scss';
 import InputGroup from './InputGroup';
-import Collapsable from './Collapsable';
 
-function Fillin() {
-  return (
-    <section className="collapsable">
-      {/*<Collapsable
-            name = 'Rellena'
-            icon='collapsable__header--icon fa fa-keyboard-o'
-       />*/}
-      <div className="fillin-content collapsable__box js-collapsable-close">
-        <InputGroup
-          htmlFor="name"
-          labelText="Nombre completo"
-          type="text"
-          id="name"
-          className="fillin__input js-input-name js-input-text"
-          placeholder="Ej: Sally Jill"
-        />
+class Fillin extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-        <InputGroup
-          htmlFor="job"
-          labelText="Puesto"
-          type="text"
-          id="job"
-          className="fillin__input js-input-job js-input-text"
-          placeholder="Ej: Front-end unicorn"
-        />
-
-        <label htmlFor="photo" className="fillin__label">
-          Imagen de perfil
-        </label>
-
-        <div className="fillin__photo">
-          <button className="fillin__photo--button js__profile-trigger">Añadir imagen</button>
-          <input
-            type="file"
-            name="photo"
-            id="img-selector"
-            className="hidden-field input-required js__profile-upload-btn"
-            required
+  render() {
+    return (
+      <section className="collapsable">
+        <div className="fillin-content collapsable__box js-collapsable-close">
+          <InputGroup
+            text="Nombre completo"
+            placeText="Ej: Sally Jill"
+            name="name"
+            handleInput={this.props.handleInput}
+            value={this.props.data.name}
           />
-          <div className="fillin__photo--preview js__profile-preview js-small-photo"></div>
+
+          <InputGroup
+            text="Puesto"
+            placeText="Ej: Front-end unicorn"
+            name="job"
+            handleInput={this.props.handleInput}
+            value={this.props.data.job}
+            className="fillin__input js-input-job js-input-text"
+          />
+
+          <label htmlFor="photo" className="fillin__label">
+            Imagen de perfil
+          </label>
+
+          <div className="fillin__photo">
+            <button className="fillin__photo--button js__profile-trigger">Añadir imagen</button>
+            <input
+              type="file"
+              name="photo"
+              id="img-selector"
+              className="hidden-field input-required js__profile-upload-btn"
+              required
+            />
+            <div className="fillin__photo--preview js__profile-preview js-small-photo"></div>
+          </div>
+
+          <InputGroup
+            htmlFor="email"
+            labelText="Email"
+            type="email"
+            id="email"
+            className="fillin__input js-input-email js-input-text"
+            placeholder="Ej: sally-hill@gmail.com"
+          />
+          <InputGroup
+            htmlFor="phone"
+            labelText="Teléfono"
+            type="text"
+            id="phone"
+            className="fillin__input js-input-phone js-input-text"
+            placeholder="Ej: 555-55-55-55"
+          />
+
+          <InputGroup
+            htmlFor="linkedin"
+            labelText="Linkedin"
+            type="text"
+            id="linkedin"
+            className="fillin__input js-input-linkedin js-input-text"
+            placeholder="Ej: linkedin.com/in/sally.hill"
+          />
+
+          <InputGroup
+            htmlFor="github"
+            labelText="GitHub"
+            type="text"
+            id="github"
+            className="fillin__input js-input-github js-input-text"
+            placeholder="Ej: @sally-hill"
+          />
         </div>
-
-        <InputGroup
-          htmlFor="email"
-          labelText="Email"
-          type="email"
-          id="email"
-          className="fillin__input js-input-email js-input-text"
-          placeholder="Ej: sally-hill@gmail.com"
-        />
-        <InputGroup
-          htmlFor="phone"
-          labelText="Teléfono"
-          type="text"
-          id="phone"
-          className="fillin__input js-input-phone js-input-text"
-          placeholder="Ej: 555-55-55-55"
-        />
-
-        <InputGroup
-          htmlFor="linkedin"
-          labelText="Linkedin"
-          type="text"
-          id="linkedin"
-          className="fillin__input js-input-linkedin js-input-text"
-          placeholder="Ej: linkedin.com/in/sally.hill"
-        />
-
-        <InputGroup
-          htmlFor="github"
-          labelText="GitHub"
-          type="text"
-          id="github"
-          className="fillin__input js-input-github js-input-text"
-          placeholder="Ej: @sally-hill"
-        />
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 export default Fillin;
--- src/components/form/InputGroup.js
+++ src/components/form/InputGroup.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
-function InputGroup(props) {
-  return (
-    <>
-      <label htmlFor={props.htmlFor} className="fillin__label">
-        {props.labelText}
-      </label>
-      <input type={props.type} id={props.id} className={props.className} placeholder={props.placeholder} required />
-    </>
-  );
+class InputGroup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleInputChange(ev) {
+    this.props.handleInput({
+      inputName: ev.target.name,
+      inputValue: ev.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <label htmlFor={this.props.forId} className="fillin__label">
+          {this.props.text}
+        </label>
+        <input
+          className={this.props.className}
+          placeholder={this.props.placeText}
+          type={this.props.type}
+          name={this.props.name}
+          onChange={this.handleInputChange}
+          value={this.props.value}
+        />
+      </>
+    );
+  }
 }
+
+InputGroup.defaultProps = {
+  type: 'text',
+  requiered: true,
+};
+
 export default InputGroup;

b1caef1adb29477236a2dba05d5b667dfb5bd278
Laura Portillo <lauraportillo.ark@gmail.com>
2021-03-02 17:02:17 +0100

    check button reset

--- src/components/preview/ResetButton.js
+++ src/components/preview/ResetButton.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import '../../stylesheets/preview/resetButton.scss';
 
-class ResetButton extends React.Component {
-  render() {
-    return (
-      <div className="preview__reset">
-        <button className="preview__reset--reset js-reset" onClick={this.props.handleReset}>
-          <i className="far fa-trash-alt"></i>
-          <a className="button__reset" href="">
-            Reset
-          </a>
-        </button>
-      </div>
-    );
-  }
-}
+const ResetButton = (props) => {
+  const handleReset = (ev) => {
+    props.handleReset();
+  };
+
+  return (
+    <div className="preview__reset">
+      <button className="preview__reset--reset js-reset" onClick={handleReset}>
+        <i className="far fa-trash-alt"></i>
+        <a className="button__reset" href="">
+          Reset
+        </a>
+      </button>
+    </div>
+  );
+};
 
 export default ResetButton;

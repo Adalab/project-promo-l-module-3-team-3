6b2cbcc94b54434508d5561de8200c87322d9c24
cvcastano <you@example.com>
2021-03-15 13:09:44 +0100

    run docs

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 const express = require('express');
+// const path = require('path');
 
 // SERVER
 
@@ -14,13 +15,16 @@ app.listen(serverPort, () => {
   console.log(`App listening at http://localhost:${serverPort}`);
 });
 
+// // config express static server
+// const staticServerPath = './public'; // relative to the root of the project
+// app.use(express.static(staticServerPath));
 
-// // API request > POST > http://localhost:3000/card
-// server.post('/card', (req, res) => {
+// API request > POST > http://localhost:3000/card
+// app.post('/card', (req, res) => {
 //   // console request body params
-//   console.log(`Creating new card: "${req.body.card}"`);
+//   console.log(`Creating new card: ${req.body}`);
 //   const response = {
-//     result: `User created: ${req.body.card}`,
+//     result: `User created: ${req.body}`,
 //   };
 //   res.json(response);
 // });
@@ -30,3 +34,17 @@ app.post("/card", (req, res) => {
 
   res.json(["hello, world"]);
 });
+
+// app.get("/card/:id/", (req, res) => {
+//   console.log(req.body);
+
+//   res.json(["ni idea"]);
+// });
+
+// // not found error
+// app.get('*', (req, res) => {
+//   // relative to this directory
+//   const notFoundFileRelativePath = '../public/404-not-found.html'; // este fichero tenemos que crearlo en server/public
+//   const notFoundFileAbsolutePath = path.join(__dirname, notFoundFileRelativePath);
+//   res.status(404).sendFile(notFoundFileAbsolutePath);
+// });

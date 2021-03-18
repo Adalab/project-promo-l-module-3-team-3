839d65f70aabbf2d442100116ad81430c27d8db8
cvcastano <you@example.com>
2021-03-18 01:06:52 +0100

    run build

--- web/src/services/fetchApi.js
+++ web/src/services/fetchApi.js
@@ -1,19 +1,16 @@
 
 // durante la clase de hoy miércoles, falta acabarlo más abajo, creo
 const isDevEnvironment = process.env.NODE_ENV === 'development'; // Booleano
-const url = isDevEnvironment ? 'http://localhost:3000' : 'https://awesome-profile-cards-lolies.herokuapp.com/#/';
+const apiUrl = isDevEnvironment ? 'http://localhost:3000' : 'https://awesome-profile-cards-lolies.herokuapp.com/#/';
 
-console.log('isDevEnvironment', isDevEnvironment);
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-// console.log('apiUrl', apiUrl);
 
 const fetchApi = (data) => {
+  console.log(apiUrl);
  
   const url = "http://localhost:3000/card";
  
   // const url = 'https://profileawesome.herokuapp.com/card';
 
-//--------------------------------------------------------------
   return fetch(url, {
     method: 'POST',
     body: JSON.stringify(data),

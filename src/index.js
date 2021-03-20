4fb585338cf0a518bfae39f36c278fc38eac0d48
cvcastano <you@example.com>
2021-03-20 16:51:09 +0100

    add \/card to url

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ app.post('/card', (req, res) => {
       response.cardURL = "http://localhost:3000/card/" + result.lastInsertRowid;
     }
     else {
-      response.cardURL = 'https://awesome-profile-cards-lolies.herokuapp.com/#/' + result.lastInsertRowid;
+      response.cardURL = 'https://awesome-profile-cards-lolies.herokuapp.com/card/' + result.lastInsertRowid;
     }
   }
 
--- web/src/services/fetchApi.js
+++ web/src/services/fetchApi.js
@@ -1,7 +1,9 @@
 
 const isDevEnvironment = process.env.NODE_ENV === 'development';
-const apiUrl = isDevEnvironment ? 'http://localhost:3000' : 'https://awesome-profile-cards-lolies.herokuapp.com';
+const apiUrl = isDevEnvironment ? 'http://localhost:3000' : 'https://awesome-profile-cards-lolies.herokuapp.com/#/';
 
+
+//`${apiUrl}/card`
 
 const fetchApi = (data) => {
 
